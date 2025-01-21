ff566d3b53948700ce586e8a9a33e33e4192cb90
PaulFesama <96030318+PaulFesama@users.noreply.github.com>
2025-01-21 14:24:33 +1000

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function closeMenu() {
+  // Close the navbar collapse after a link is clicked
+  $('.navbar-collapse').collapse('hide');
+}
+
+// btn clicked 
+function scrollToSkills() {
+  // Scroll to the skills section
+  document.getElementById('skills').scrollIntoView({ behavior: 'smooth' });
+}
